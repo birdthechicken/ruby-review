17b1d500dfe1cfbe37951eb45f3fae0f5076994d
birdthechicken <sinangundogar61@gmail.com>
2019-04-23 17:08:41 -0500

    day2

---
+++ loops.rb
@@ -0,0 +1,51 @@
+
+cities = ["Jakcson", "Southhaven", "Oxford", "Vicksburg", "Booneville"]
+
+# index= 0
+
+# cities.length.times do 
+
+#   city = cities[index]
+#   puts "#{ city }, Mississippi"
+#   index += 1
+# end
+
+  
+
+# cities.length.times do |index|
+
+#   city = cities[index]
+#   puts "#{ city }, Mississippi"
+ 
+# end
+
+
+# cities.each do |city|
+
+#   puts "#{ city }, Mississippi"
+ 
+# end
+
+
+number = 0
+
+
+# while number < 4
+#   puts "bats in the belfrey"
+#   number += 1
+# end
+
+# unless number > 4
+#   puts "bats in the belfrey"
+  
+# end
+
+
+until number > 4
+  puts "bats in the belfrey"
+  number += 1
+end
+
+
+
+
